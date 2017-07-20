0b2c457388745228fbcbadfef67edadd468da99c
Alexander Lamb <lambalex@leto22.iro.umontreal.ca>
2017-07-20 01:24:34 -0400

    updates

--- layers.py
+++ layers.py
@@ -14,10 +14,13 @@ Initially all visible, use batch norm in generator and inference.  Inference and
 '''
 
 import torch
+from torch import optim
 import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 import torchvision.datasets as dsets
+import gzip
+import cPickle as pickle
 
 class Generator_Low(nn.Module):
 
@@ -40,39 +43,69 @@ class Generator_Low(nn.Module):
 
         return xgen
 
-class Discriminator_Low(nn.module):
+class Discriminator_Low(nn.Module):
 
-    def __init__(self, z_dim, h_dim, x_dim):
+    def __init__(self, h_dim, x_dim):
         super(Discriminator_Low, self).__init__()
-        self.fc_1 = nn.Linear(z_dim + x_dim, h_dim)
+        self.fc_1 = nn.Linear(x_dim, h_dim)
         self.fc_2 = nn.Linear(h_dim, h_dim)
         self.fc_3 = nn.Linear(h_dim, 1)
-        self.z_dim = z_dim
         self.activ = nn.LeakyReLU()
 
-    def forward(self, x, z):
-        inp_concat = torch.cat((x,z), 1)
-        h1 = self.activ(self.bn_h(self.fc_1(inp_concat)))
-        h2 = self.activ(self.bn_h(self.fc_1(h1)))
+    def forward(self, x):
+        inp_concat = x#torch.cat((x,z), 1)
+        print inp_concat.size()
+        h1 = self.activ(self.fc_1(inp_concat))
+        h2 = self.activ(self.fc_2(h1))
         out = self.fc_3(h2)
 
         return out
 
 if __name__ == "__main__":
     
+    mn = gzip.open("/u/lambalex/data/mnist/mnist.pkl.gz")
+
+    train, valid, test = pickle.load(mn)
+
+    trainx,trainy = train
+    validx,validy = valid
+    testx, testy = test
+
     generator_low = Generator_Low(128,1024,28*28)
 
     generator_low.cuda()
 
-    discriminator_low = Discriminator_Low(128, 1024, 28*28)
+    discriminator_low = Discriminator_Low(1024, 28*28)
 
     discriminator_low.cuda()
 
+    gparams = generator_low.parameters()
+    dparams = discriminator_low.parameters()
+
     z_in = Variable(torch.randn(64,128).cuda())
 
-    output = generator_low(z_in)
+    xgen = generator_low(z_in)
 
-    print output.size()
+    disc_out_gen = discriminator_low(xgen)
+
+    xreal = Variable(torch.from_numpy(trainx[0:64]).cuda())
+
+    disc_out_real = discriminator_low(xreal)
+
+    print disc_out_gen.size(), disc_out_real.size()
+
+    gen_loss = ((disc_out_gen - 0.5)**2).sum()/64.0
+
+    disc_loss = ((disc_out_gen)**2 + (disc_out_real - 1.0)**2).sum()/64.0
+
+    optimizer_gen = optim.RMSprop(gparams)
+    optimizer_disc = optim.RMSprop(dparams)
+
+    
+    for i in range(0,1000):
+
+        optimizer_gen.zero_grad()
+        optimizer_disc.zero_grad()
 
 
 
